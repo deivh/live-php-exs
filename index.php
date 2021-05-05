494491b070277e10bd592ede9be70b5178840577
Davide Mannarelli <davidemannarelli24@gmail.com>
2021-05-05 10:33:14 +0200

    php snacks: foreach

--- index.php
+++ index.php
@@ -8,92 +8,142 @@
     <title>PhP Test</title>
 </head>
 <body>
-           
     <div class="row">
-    <!-- ESERCIZIO 1 -->
-                 <!-- Creare un array con 15 numeri casuali, 
-                    tenendo conto che l'array non dovrà 
-                    contenere lo stesso numero più di una volta-->
-            <?php
-            $array = [];
-            while(count($array)<15) {
-                $num = rand(1,100);
-                if (!in_array($num, $array)) {
-                    $array[]= $num;
-                }
-            }
-            for ($i=0; $i <15 ; $i++) { 
-                echo $array[$i];
-                echo ' ';
-            }
-        ?>
+    <?php
+        // Creiamo un array contenente le partite di 
+                    // basket di un'ipotetica tappa del calendario. 
+                    // Ogni array avrà una squadra di casa e una squadra ospite, 
+                    // punti fatti dalla squadra di casa e punti fatti 
+                    // dalla squadra ospite. Stampiamo a schermo tutte 
+                    // le partite con questo schema.
+                    $db = [
+                        [
+                            'team1' => 'Milan',
+                            'team2' => 'Torino',
+                            'score1' => 30,
+                            'score2' => 25
+                        ],
+                        [
+                            'team1' => 'Trento',
+                            'team2' => 'Bolzano',
+                            'score1' => 50,
+                            'score2' => 20
+                        ],
+                        [
+                            'team1' => 'Bologna',
+                            'team2' => 'Empoli',
+                            'score1' => 10,
+                            'score2' => 22
+                        ]
+                    ];
+                    for ($i=0;$i<count($db);$i++) {
+                        $match = $db[$i];
+                        echo 'team 1: ' . $match['team1'] . '<br>'
+                            . 'team 2: ' . $match['team2'] . '<br>'
+                            . 'score 1: ' . $match['score1'] . '<br>'
+                            . 'score 2: ' . $match['score2'] . '<br><hr>';
+                    }
+    ?>
     </div>
 
     <div class="row">
-    <!-- ESERCIZIO 2 -->
-               <!-- // Prendere un paragrafo abbastanza lungo, contenente 
-                // diverse frasi. Prendere il paragrafo e suddividerlo 
-                // in tanti paragrafi. Ogni punto un nuovo paragrafo. -->
+    
+    <?php
+        // Passare come parametri GET name, mail e age e verificare 
+                    // (cercando i metodi che non conosciamo nella documentazione) 
+                    // che name sia più lungo di 3 caratteri, che mail contenga 
+                    // un punto e una chiocciola e che age sia un numero. Se 
+                    // tutto è ok stampare "Accesso riuscito", altrimenti 
+                    // "Accesso negato"
+                    $name = $_GET['name'];
+                    $mail = $_GET['mail'];
+                    $age = $_GET['age'];
+                    if (strlen($name) < 3 
+                        || strpos($mail, '.') === false
+                        || strpos($mail, '@') === false
+                        || !is_numeric($age)) {
+                        echo "Accesso negato";
+                    } else {
+                        echo "Accesso riuscito";
+                    }
+    ?>
 
-            <?php
-            
-            $text = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam sed, corporis porro a nam earum dolorum, cumque praesentium, veniam impedit sequi provident sit laudantium illo? Laborum voluptatem repellendus eius illum.";
-            $cutText = explode('.',$text);
-            for ($i=0; $i < count($cutText); $i++) { 
-                echo "<p>" . $cutText[$i] . "." . "</p>";
-            }
-            ?>
     </div>
 
 
+
     <div class="row">
-            <!-- ESERCIZIO 3
-            // Utilizzare questo array: https://pastebin.com/CkX3680A. 
-        // Stampiamo il nostro array mettendo gli insegnanti in un 
-        // rettangolo grigio e i PM in un rettangolo verde. -->
         <?php
+            $arr1 = [ 1, 2, 3, 4, 5 ];
+            // foreach ($arr1 as $value) {
+            //     echo $value . '<br>';   
+            // }
+            $arr2 = [
+                'key' => 'value',
+                'subArr' => [
+                    1, 2, 3, 4
+                ],
+                'value' => 100
+            ];
+            // foreach ($arr2 as $key => $value) {
+            //     echo $key . ' => ' . $value . '<br>';
+            // }
+            $arr3 = [
+                'teachers' => ['Fabio', 'Michele'],
+                'PM' => ['Roberto']
+            ];
+            foreach ($arr3 as $type => $names) {
+                echo $type . ': ';
+                foreach ($names as $name) {
+                    echo $name . ' ';
+                }
+                echo '<br>';
+            }
 
-                $db = [
-                    'teachers' => [
-                        [
-                            'name' => 'Michele',
-                            'lastname' => 'Papagni'
-                        ],
-                        [
-                            'name' => 'Fabio',
-                            'lastname' => 'Forghieri'
-                        ]
-                    ],
-                    'pm' => [
-                        [
-                            'name' => 'Roberto',
-                            'lastname' => 'Marazzini'
-                        ],
-                        [
-                            'name' => 'Federico',
-                            'lastname' => 'Pellegrini'
-                        ]
-                    ]
-                ];
-                $teachers = $db['teachers'];
-                $pms = $db['pm'];
-                var_dump(array_keys($db)); die();
-                // var_dump($teachers);
-                // echo '<br>';
-                // var_dump($pms);
-                for ($i=0;$i<count($teachers);$i++) {
-                    $teacher = $teachers[$i];
-                    echo '<div class="box teachers">' 
-                        . $teacher['name'] . ' ' . $teacher['lastname']
-                        . '</div>';
-                }
-                for ($i=0;$i<count($pms);$i++) {
-                    $pm = $pms[$i];
-                    echo '<div class="box pm">' 
-                        . $pm['name'] . ' ' . $pm['lastname']
-                        . '</div>';
-                }
-        ?>
+       ?>
+    
+    </div>
+    
+    <div class="row">
+            <?php
+               $arr1 = [ 1, 2, 3, 4, 5 ];
+               foreach ($arr1 as $value) {
+                   echo $value . '<br>';   
+               }
+               $arr2 = [
+                   'key' => 'value',
+                   'subArr' => [
+                       1, 2, 3, 4
+                   ],
+                   'value' => 100
+               ];
+               foreach ($arr2 as $key => $value) {
+                   echo $key . ' => ' . $value . '<br>';
+               }
+               $arr3 = [
+                   'teachers' => ['Fabio', 'Michele'],
+                   'PM' => ['Roberto']
+               ];
+               foreach ($arr3 as $type => $names) {
+                   echo $type . ': ';
+                   foreach ($names as $name) {
+                       echo $name . ' ';
+                   }
+                   echo '<br>';
+               }
+               // // --- OLD SCHOOL
+               // $arr3Keys = array_keys($arr3);
+               // for ($i=0;$i<count($arr3Keys);$i++) {
+               //     $type = $arr3Keys[$i];
+               //     $names = $arr3[$type];
+               //     echo $type . ': ';
+               //     for($x=0;$x<count($names);$x++) {
+               //         $name = $names[$x];
+               //         echo $name . ' ';
+               //     }
+               //     echo '<br>';
+               // } 
+            ?>
     </div>
 </body>
 
